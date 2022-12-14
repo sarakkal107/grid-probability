c8881c177e34003d341077fdd72fdde9b840ea9f
ssa150 <31035035+sarakkal107@users.noreply.github.com>
2022-12-13 21:39:52 -0500

    Update visualize.py
    Fixed visualize row/column error

--- visualize.py
+++ visualize.py
@@ -7,49 +7,63 @@ class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
+        App.initialize(self)
+        App.grid(self)
+        App.show_path(self)
+
+    def initialize(self):
+
         folder = "maps"
         mapFile = open(folder + "/map1")
-        mapText = []
+        self.mapText = []
         for line in mapFile.readlines():
             line = line.strip()
-            mapText.append(line.split(' '))
+            self.mapText.append(line.split(' '))
         mapFile.close
 
         dataFile = open(folder + "/map1_data3.txt", 'r')
-        dataText = dataFile.read().splitlines()
+        self.dataText = dataFile.read().splitlines()
         dataFile.close()
 
-        self.rows = len(mapText[0])
-        self.columns = len(mapText)
-        self.cellwidth = 16
-        self.cellheight = 16
+        self.rows = len(self.mapText)
+        self.columns = len(self.mapText[0])
+        self.cellwidth = 10
+        self.cellheight = 10
 
-        w = self.columns*self.cellwidth+2*self.cellwidth
-        h = self.rows*self.cellheight+120
+        self.w = self.columns*self.cellwidth+2*self.cellwidth + 500
+        self.h = self.rows*self.cellheight+120
 
-        if (w < 300):
-            w = 300
-        if (h < 300):
-            h = 300
+        if (self.w < 300):
+            self.w = 300
+        if (self.h < 300):
+            self.h = 300
 
-        actions = dataText[self.columns+4]
-        observations = dataText[self.columns+6]
+        self.actions = self.dataText[self.rows+4]
+        # print(self.actions)
+        self.observations = self.dataText[self.rows+6]
 
-        path = []
-        for i in range(self.columns):
-            path.append(tuple(map(int, dataText[i+3].split())))
+        self.path = [tuple(map(int, self.dataText[1].split()))]
+        for i in range(self.rows):
+            self.path.append(tuple(map(int, self.dataText[i+3].split())))
         # print(path)
 
         self.canvas = tk.Canvas(
-            self, width=w, height=h, borderwidth=0, highlightthickness=0)
+            self, width=self.w, height=self.h, borderwidth=0, highlightthickness=0)
         self.canvas.pack(side="top", fill="both", expand="true")
         self.title("Data Visualization")
+
+        self.frame = tk.Frame(self.canvas, background="#ffffff")
+        self.vsb = tk.Scrollbar(self, orient="vertical",
+                                command=self.canvas.yview)
+        self.canvas.configure(yscrollcommand=self.vsb.set)
+        self.vsb.pack(side="right", fill="y")
+        self.canvas.pack(side="left", fill="both", expand=True)
 
         self.rect = {}
         self.oval = {}
         self.lines = {}
 
-        # Generate Grid
+    def grid(self):
         for column in range(self.columns):
             for row in range(self.rows):
                 x1 = column * self.cellwidth + self.cellwidth
@@ -57,11 +71,11 @@ class App(tk.Tk):
                 x2 = x1 + self.cellwidth
                 y2 = y1 + self.cellheight
                 color = ""
-                if (mapText[column][row] == "N"):
+                if (self.mapText[row][column] == "N"):
                     color = "#E1E7FF"
-                elif (mapText[column][row] == "H"):
+                elif (self.mapText[row][column] == "H"):
                     color = "#FDFFE1"
-                elif (mapText[column][row] == "T"):
+                elif (self.mapText[row][column] == "T"):
                     color = "#FFE1FF"
                 else:
                     color = "red"
@@ -69,78 +83,67 @@ class App(tk.Tk):
                 self.rect[row, column] = self.canvas.create_rectangle(
                     x1, y1, x2, y2, fill=color, tags="rect")
 
-                #print(str(x1) + "," + str(y1) + " -> " + str(x2) + "," + str(y2))
-
+    def show_path(self):
         # Generate Lines
-        for i in range(len(actions)-1):
-            x1 = (path[i][0]+1) * self.cellwidth + self.cellwidth/2
-            y1 = (path[i][1]+1) * self.cellheight + self.cellheight/2
-            x2 = (path[i+1][0]+1) * self.cellwidth + self.cellwidth/2
-            y2 = (path[i+1][1]+1) * self.cellheight + self.cellheight/2
+        # print(len(self.path))
+        for i in range(len(self.actions)-1):
+            y1 = (self.path[i][0]+1) * self.cellwidth + self.cellwidth/2
+            x1 = (self.path[i][1]+1) * self.cellheight + self.cellheight/2
+            y2 = (self.path[i+1][0]+1) * self.cellwidth + self.cellwidth/2
+            x2 = (self.path[i+1][1]+1) * self.cellheight + self.cellheight/2
+            # print(str(x1) + "," + str(y1) + " -> " + str(x2) + str(y2))
             self.lines = self.canvas.create_line(
                 x1, y1, x2, y2, fill="green", width=3)
 
-            #print(str(x1) + "," + str(y1) + " -> " + str(x2) + "," + str(y2))
-
-        # Generate Points
-        for item in path:
-            x1 = (item[0]+1) * self.cellwidth + self.cellwidth/4
-            y1 = (item[1]+1) * self.cellheight + self.cellheight/4
+        # Plot Points
+        for item in self.path:
+            y1 = (item[0]+1) * self.cellwidth + self.cellwidth/4
+            x1 = (item[1]+1) * self.cellheight + self.cellheight/4
             x2 = x1 + self.cellwidth - self.cellwidth/4*2
             y2 = y1 + self.cellheight - self.cellheight/4*2
             self.oval = self.canvas.create_oval(
                 x1, y1, x2, y2, fill="#F0FFF0", tags="point")
 
-            #print(str(x1) + "," + str(y1) + " -> " + str(x2) + "," + str(y2))
-
         # Recolor first point
-        x1 = (path[0][0]+1) * self.cellwidth + self.cellwidth/4
-        y1 = (path[0][1]+1) * self.cellheight + self.cellheight/4
+        y1 = (self.path[0][0]+1) * self.cellwidth + self.cellwidth/4
+        x1 = (self.path[0][1]+1) * self.cellheight + self.cellheight/4
         x2 = x1 + self.cellwidth - self.cellwidth/4*2
         y2 = y1 + self.cellheight - self.cellheight/4*2
         self.oval = self.canvas.create_oval(
             x1, y1, x2, y2, fill="#FFBF00", tags="point")
 
         # Recolor last point
-        x1 = (path[len(path)-1][0]+1) * self.cellwidth + self.cellwidth/4
-        y1 = (path[len(path)-1][1]+1) * self.cellheight + self.cellheight/4
+        y1 = (self.path[len(self.path)-1][0]+1) * \
+            self.cellwidth + self.cellwidth/4
+        x1 = (self.path[len(self.path)-1][1]+1) * \
+            self.cellheight + self.cellheight/4
         x2 = x1 + self.cellwidth - self.cellwidth/4*2
         y2 = y1 + self.cellheight - self.cellheight/4*2
         self.oval = self.canvas.create_oval(
             x1, y1, x2, y2, fill="#74C365", tags="point")
-        initial = tuple(map(int, dataText[1].split()))
 
-        # print(path)
-
+        # Legend
         self.canvas.create_text(
-            w/2, h-80, text="Actions: " + actions, font=('Helvetica 11 bold'), fill="black")
-        self.canvas.create_text(w/2, h-65, text="Observations: " +
-                                observations, font=('Helvetica 11 bold'), fill="black")
+            self.w/2, self.h-80, text="Actions: " + self.actions, font=('Helvetica 11 bold'), fill="black")
+        self.canvas.create_text(self.w/2, self.h-65, text="Observations: " +
+                                self.observations, font=('Helvetica 11 bold'), fill="black")
 
         self.canvas.create_rectangle(
-            0+self.cellwidth, h-50, (w-30)/4*1, h-30, fill="#E1E7FF")
-        self.canvas.create_rectangle(
-            (w-30)/4*1, h-50, (w-30)/4*2, h-30, fill="#FDFFE1")
-        self.canvas.create_rectangle(
-            (w-30)/4*2, h-50, (w-30)/4*3, h-30, fill="#FFE1FF")
-        self.canvas.create_rectangle(
-            (w-30)/4*3, h-50, w-self.cellwidth, h-30, fill="red")
-
-        self.canvas.create_text((((w-30)/4*1) + 15)/2, h-40, text="N",
-                                font=('Helvetica 11 bold'), fill="black")
-        self.canvas.create_text((((w-30)/4*1) + ((w-30)/4*2))/2, h-40, text="H",
-                                font=('Helvetica 11 bold'), fill="black")
-        self.canvas.create_text((((w-30)/4*2) + ((w-30)/4*3))/2, h-40, text="T",
-                                font=('Helvetica 11 bold'), fill="black")
-        self.canvas.create_text((((w-30)/4*3) + ((w-30)/4*4))/2, h-40, text="B",
-                                font=('Helvetica 11 bold'), fill="black")
-
-        self.canvas.create_rectangle(
-            0+self.cellwidth, h-30, w-self.cellwidth, h-10, fill="black")
-        self.canvas.create_text(w/3*1, h-20, text="Starting Point",
-                                font=('Helvetica 11 bold'), fill="#FFBF00")
-        self.canvas.create_text(w/3*2, h-20, text="Ending Point",
-                                font=('Helvetica 11 bold'), fill="#74C365")
+            self.w-400, self.h/2-120, self.w-100, self.h/2+120, fill="black")
+        self.canvas.create_text(
+            self.w-250, self.h/2-70, text="Legend", font=('Helvetica 24'), fill="white")
+        self.canvas.create_text(
+            self.w-250, self.h/2-40, text="Starting Point", font=('Helvetica 16'), fill="#FFBF00")
+        self.canvas.create_text(
+            self.w-250, self.h/2-20, text="Ending Point", font=('Helvetica 16'), fill="#74C365")
+        self.canvas.create_text(
+            self.w-250, self.h/2, text="Normal Cell", font=('Helvetica 16'), fill="#E1E7FF")
+        self.canvas.create_text(
+            self.w-250, self.h/2+20, text="Highway Cell", font=('Helvetica 16'), fill="#FDFFE1")
+        self.canvas.create_text(
+            self.w-250, self.h/2+40, text="Hard to Traverse Cell", font=('Helvetica 16'), fill="#FFE1FF")
+        self.canvas.create_text(
+            self.w-250, self.h/2+60, text="Blocked Cell", font=('Helvetica 16'), fill="red")
 
 
 if __name__ == "__main__":
