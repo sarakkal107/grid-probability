28bae0fb58c09aee5a32aaf5cb611cac37d4bba8
Mohammad Awais Zubair <73324625+awais-zubair@users.noreply.github.com>
2022-12-14 20:39:04 -0500

    Bug Fixes

--- problem_7/mdp.py
+++ problem_7/mdp.py
@@ -12,7 +12,7 @@ class MDP:
     def __init__(self):
         self.states = self.init_states()
         self.transition_model = self.init_t_model()
-        self.Discount = .90
+        self.Discount = 0.90
 
     def init_states(self):
         s = dict()
@@ -25,20 +25,20 @@ class MDP:
     # from transition model given in problem statement
     def init_t_model(self):
         t = dict()
-        t[(1, 1, 1)] = .2
-        t[(1, 1, 2)] = .8
-        t[(1, 2, 1)] = .2
-        t[(1, 2, 4)] = .8
-        t[(2, 2, 2)] = .2
-        t[(2, 2, 3)] = .8
-        t[(2, 3, 2)] = .2
-        t[(2, 3, 1)] = .8
+        t[(1, 1, 1)] = 0.2
+        t[(1, 1, 2)] = 0.8
+        t[(1, 2, 1)] = 0.2
+        t[(1, 2, 4)] = 0.8
+        t[(2, 2, 2)] = 0.2
+        t[(2, 2, 3)] = 0.8
+        t[(2, 3, 2)] = 0.2
+        t[(2, 3, 1)] = 0.8
         t[(3, 4, 2)] = 1
         t[(3, 3, 4)] = 1
-        t[(4, 1, 4)] = .1
-        t[(4, 1, 3)] = .9
-        t[(4, 4, 4)] = .2
-        t[(4, 4, 1)] = .8
+        t[(4, 1, 4)] = 0.1
+        t[(4, 1, 3)] = 0.9
+        t[(4, 4, 4)] = 0.2
+        t[(4, 4, 1)] = 0.8
         return t
 
     def get_states(self):
